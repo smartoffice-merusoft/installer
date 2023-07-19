f1b62ab5333aa3c1d2ad4399db863c0657c60e73
lysander69 <lysander69@yandex.ru>
2023-07-19 08:29:51 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt update
 apt install wget software-properties-common  curl gpg gnupg2 software-properties-common apt-transport-https lsb-release ca-certificates -y
 
-curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg -y
 
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |tee  /etc/apt/sources.list.d/pgdg.list
 
