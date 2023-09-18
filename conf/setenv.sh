958f9b3da9a42a1ccb77d65bb5da9fb731670789
smartoffice-merusoft <139943831+smartoffice-merusoft@users.noreply.github.com>
2023-09-18 17:09:35 +0300

    Create setenv.sh

---
+++ conf/setenv.sh
@@ -0,0 +1,4 @@
+export CATALINA_OPTS="$CATALINA_OPTS -Xms512m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m"
+export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dlog4j2.formatMsgNoLookups=true"
